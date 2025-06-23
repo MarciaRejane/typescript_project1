3a50e4e9a7dd3fb237926c419ed7a028900babd7
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-23 13:27:21 -0300

    Questão 2 finalizada

---
+++ src/Exercicio1/questao2.ts
@@ -0,0 +1,12 @@
+// Podem ser atualizada e declaradas novamente dentro de seu escopo.
+var nome1: string = "José"
+
+// Podem ser atualizadas mas não declaradas novamente.
+let nome2: string = "Maria";
+
+// Não podem ser declaradas novamente e nem atualizadas, elas são imutaveis.
+const numero: number = 80;
+
+console.log(nome1);
+console.log(nome2);
+console.log(numero);
