e9444c718b0ba335edcb51913d767c574ce341d0
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-25 14:49:23 -0300

    Questão 4 finalizada

---
+++ src/Exercicio1/questao4.ts
@@ -0,0 +1,13 @@
+let palavra: number = 80;
+
+// palavra = "80";
+
+
+//A mensagem de verificação
+// src/Exercicio1/questao4.ts:3:1 - error TS2322: Type 'string' is not assignable to type 'number'.
+
+// 3 palavra = "80";
+//   ~~~~~~~
+
+
+// Found 1 error in src/Exercicio1/questao4.ts:3
