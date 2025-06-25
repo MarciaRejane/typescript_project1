5edc0c693af216ce3943d29c392017fc1d87c1db
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-25 15:10:02 -0300

    Questão 5 finalizada

---
+++ src/Exercicio1/questao5.ts
@@ -0,0 +1,14 @@
+let string: string = "Essa variavel é uma string";
+console.log(string);
+// Mensagem escrevendo uma string
+//  Essa variavel é uma string
+
+
+ /*colocando numeros em vez de string
+  string = 1235;*/
+
+/*A mensagem da forma errada
+ error TS2322: Type 'number' is not assignable to type 'string'.
+
+ 7 string = 1235;
+   ~~~~~~ */
