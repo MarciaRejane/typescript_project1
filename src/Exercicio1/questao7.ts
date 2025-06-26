23d4f2b16294efdc724dea17c1dd697b2284d9d8
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-26 10:09:20 -0300

    Questão 7 finalizada

---
+++ src/Exercicio1/questao7.ts
@@ -0,0 +1,11 @@
+//Adiciona um ou mais elementos no final da array
+let arr: number[] = [ 1, 2, 4];
+arr.push(5,6);
+
+//Remove o primeiro elemento da array
+let primeiro = arr.shift();
+
+//Interar pelo array.
+for (const elemento of arr) {
+    console.log(elemento);
+}
