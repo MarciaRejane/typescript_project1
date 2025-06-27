dc36f514131f156ea8cbf0156f058a3f09802acd
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-27 20:20:40 -0300

    Questão 15 finalizada

---
+++ src/Exercicio1/questao15.ts
@@ -0,0 +1,8 @@
+let array: [number, string] = [30, "João"];
+
+
+const numeros = array[0] as number;
+const texto = array[1] as string;
+
+console.log("numero:", numeros );
+console.log("strings:", texto);
