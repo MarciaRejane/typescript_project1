96ec0679850c41be81c1292b48fe9c0305d953f7
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-29 23:01:14 -0300

    Corrigindo a questão 11

--- src/Exercicio1/questao11.ts
+++ src/Exercicio1/questao11.ts
@@ -1,9 +1,13 @@
-function combine(valor1: boolean, valor2: number): boolean | number {
-    if (valor1) {
-        return valor2; 
+function combine(valor1: number | boolean, valor2: number | boolean): boolean | number {
+    if (typeof valor1 === 'boolean' && typeof valor2 === 'number') {
+        return valor1; 
+    } else if(typeof valor1 === 'number' && typeof valor2 === 'boolean'){
+        return valor2;  
     } else {
-        return false;  
+        return valor1;
     }
 }
 console.log(combine(true, 10));
 console.log(combine(false, 5));
+console.log(combine(7,15));
+console.log(combine(true,false));
