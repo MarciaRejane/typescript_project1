682112ef6cdfec9e84827b71ad466b0030007945
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-06-29 22:42:30 -0300

    Corrigindo a questão 6

--- src/Exercicio1/questao6.ts
+++ src/Exercicio1/questao6.ts
@@ -1,4 +1,8 @@
 let numerostring: string = "12357"
 let mudarNumero: number = parseInt(numerostring);
-
 console.log(`Convertendo para numero: ${mudarNumero}`);
+
+let resultado1: number = mudarNumero +10;
+let resultado2: number = mudarNumero +20;
+console.log(`Resultado da operação que é number ${resultado1}`);
+console.log(`Resultado da operação que é number ${resultado2}`);
